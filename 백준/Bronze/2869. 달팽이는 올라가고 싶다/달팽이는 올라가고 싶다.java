b66299b3992cdb40039b1c8dd5dc46bccb261f3c
유용준 <144577722+dydwns6837@users.noreply.github.com>
2025-12-02 14:26:20 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.java
@@ -9,12 +9,7 @@ public class Main {
 		int A = Integer.parseInt(st.nextToken());
 		int B = Integer.parseInt(st.nextToken());
 		int V = Integer.parseInt(st.nextToken());
-		int answer = 0;
-		if ((V - B) % (A - B) == 0) {
-			answer = (V - B) / (A - B);
-		} else {
-			answer = (V - B) / (A - B) + 1;
-		}
+		int answer = (int)Math.ceil((double)(V - A) / (A - B)) + 1;
 		System.out.println(answer);
 	}
 
