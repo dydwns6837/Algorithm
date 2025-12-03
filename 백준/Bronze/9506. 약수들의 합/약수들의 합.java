af340b726b19494be322ab82d57720badfe2f794
유용준 <144577722+dydwns6837@users.noreply.github.com>
2025-12-03 09:31:11 +0900

    [Bronze I] Title: 약수들의 합, Time: 124 ms, Memory: 15648 KB -BaekjoonHub

---
+++ 백준/Bronze/9506. 약수들의 합/약수들의 합.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n;
+		StringBuilder sb = new StringBuilder();
+		while ((n = Integer.parseInt(br.readLine())) != -1) {
+			List<Integer> list = new ArrayList<>();
+			int sum = 0;
+			for (int i = 1; i < n; i++) {
+				if (n % i == 0) {
+					sum += i;
+					list.add(i);
+				}
+			}
+			if (sum == n) {
+				sb.append(n + " = ");
+				for (int i = 0; i < list.size(); i++) {
+					if (i != list.size() - 1) {
+						sb.append(list.get(i) + " + ");	
+					} else {
+						sb.append(list.get(i)).append("\n");
+					}
+				}
+			} else {
+				sb.append(n + " is NOT perfect.").append("\n");
+			}
+		}
+		System.out.println(sb);
+
+	}
+
+}
