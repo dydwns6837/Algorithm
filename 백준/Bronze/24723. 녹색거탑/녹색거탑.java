b67a95834ecd1f049c0863fc2c28a60352ad065c
유용준 <144577722+dydwns6837@users.noreply.github.com>
2025-12-22 23:21:42 +0900

    [Bronze IV] Title: 녹색거탑, Time: 104 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,12 @@
+import java.io.*;
+public class Main {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int answer = 1;
+        for (int i = 0; i < n; i++) {
+            answer *= 2;
+        }
+        System.out.println(answer);
+    }
+}
