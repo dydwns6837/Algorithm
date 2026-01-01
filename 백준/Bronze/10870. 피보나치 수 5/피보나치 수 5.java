18848108604854c46fc8886a260a9d39e9959bb3
유용준 <144577722+dydwns6837@users.noreply.github.com>
2026-01-01 22:49:43 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 108 ms, Memory: 14192 KB -BaekjoonHub

---
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main {
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int n = Integer.parseInt(br.readLine());
+ 
+		System.out.println(fibona(n));
+ 
+	}
+ 
+	static int fibona(int n) {
+		if (n == 0)	{
+            return 0;
+        }
+		if (n == 1)	{
+            return 1;
+        }
+		return fibona(n - 1) + fibona(n - 2);
+	}
+}
