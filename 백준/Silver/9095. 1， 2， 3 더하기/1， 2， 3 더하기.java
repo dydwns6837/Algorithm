9550106af8bf4eea5ecc0978c097be20e7dc4119
유용준 <144577722+dydwns6837@users.noreply.github.com>
2026-02-12 15:58:25 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 88 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.java
@@ -1,25 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
 
+    static int[] dx = {1, 2, 3};
     public static void main(String[] args) throws IOException {
         // 백준 9095 1,2,3 더하기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        int[] dp = new int [12];
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            dp[0] = 1;
-            dp[1] = 1;
-            dp[2] = 2;
-            for (int i = 3; i < dp.length; i++) {
-                dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
-            }
-            sb.append(dp[n]).append("\n");
+            sb.append(bfs(n)).append("\n");
         }
         System.out.println(sb);
+    }
+
+    private static int bfs(int n) {
+        int cnt = 0;
+        Deque<Integer> deq = new ArrayDeque<>();
+        deq.add(0);
+        while (!deq.isEmpty()) {
+            int parent = deq.poll();
+            for (int i = 0; i < dx.length; i++) {
+                int nx = parent + dx[i];
+                if (nx > 0 && nx <= n) {
+                    if (nx == n) {
+                        cnt++;
+                    } else {
+                        deq.add(nx);
+                    }
+                }
+            }
+        }
+        return cnt;
     }
 }
