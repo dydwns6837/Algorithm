0241f0e3266b632876253cffb667cfae7a6c5335
유용준 <144577722+dydwns6837@users.noreply.github.com>
2026-03-09 12:14:23 +0900

    [Silver II] Title: 1, 2, 3 더하기 3, Time: 132 ms, Memory: 22192 KB -BaekjoonHub

--- 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
+++ 백준/Silver/15988. 1， 2， 3 더하기 3/1， 2， 3 더하기 3.java
@@ -10,14 +10,15 @@ public class Main {
         int t = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
         long[] dp = new long [1000001];
+
+        dp[0] = 1;
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
+        }
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            dp[0] = 1;
-            dp[1] = 1;
-            dp[2] = 2;
-            for (int i = 3; i < dp.length; i++) {
-                dp[i] = (dp[i - 1] + dp[i - 2] + dp[i - 3]) % 1000000009;
-            }
             sb.append(dp[n]).append("\n");
         }
         System.out.println(sb);
